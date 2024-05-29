87875c5c82850a55efb82e30021acba6feab5bde
Federico Pessina <federicopessina.work@gmail.com>
2024-05-29 16:17:00 +0200

    Create test_[iscrizioni_eventi]_iscrizione_ad_evento_non_registrato.sql

---
+++ tests/test_[iscrizioni_eventi]_iscrizione_ad_evento_non_registrato.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.iscrizioni_eventi (
+    data, ruolo, "[utenti]_username", "[stato_eventi]_stato", "[eventi]_id", "[esito_iscrizioni]_esito")
+VALUES 
+    ('2024-06-15', 'arbitro', 'fthomas1', 'CONFERMATO', 99, 'da verificare')
