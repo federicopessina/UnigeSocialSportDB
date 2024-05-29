14474f4431448f575ba322a24d1af2111841aa85
Federico Pessina <federicopessina.work@gmail.com>
2024-05-29 12:44:33 +0200

    refactor : modifica nome regola
    rule_ins_stato_e_chiuso_impossibile_nuove_iscrizioni aggiunto rule_

--- ins_stato_e_chiuso_impossibile_nuove_iscrizioni.sql
+++ ins_stato_e_chiuso_impossibile_nuove_iscrizioni.sql
@@ -1,12 +1,12 @@
--- Rule: ins_stato_e_chiuso_impossibile_nuove_iscrizioni ON public.iscrizioni_eventi
+-- Rule: rule_ins_stato_e_chiuso_impossibile_nuove_iscrizioni ON public.iscrizioni_eventi
 
--- DROP Rule IF EXISTS ins_stato_e_chiuso_impossibile_nuove_iscrizioni ON public.iscrizioni_eventi;
+-- DROP Rule IF EXISTS rule_ins_stato_e_chiuso_impossibile_nuove_iscrizioni ON public.iscrizioni_eventi;
 
-CREATE OR REPLACE RULE ins_stato_e_chiuso_impossibile_nuove_iscrizioni AS
+CREATE OR REPLACE RULE rule_ins_stato_e_chiuso_impossibile_nuove_iscrizioni AS
     ON INSERT TO public.iscrizioni_eventi
     WHERE (( SELECT eventi.e_aperto
            FROM eventi
           WHERE ((eventi.id)::text = (new."[eventi]_id")::text)) = false)
-    DO NOTHING;
+    DO INSTEAD NOTHING;
 
-COMMENT ON RULE ins_stato_e_chiuso_impossibile_nuove_iscrizioni ON public.iscrizioni_eventi IS 'Lo stato	di un evento sportivo diventa CHIUSO quando si raggiunge un numero di giocatori	pari a quello previsto dalla categoria (la	presenza dell’arbitro è opzionale); in tal caso,	non	è più possibile effettuare iscrizioni all’evento.';
+COMMENT ON RULE rule_ins_stato_e_chiuso_impossibile_nuove_iscrizioni ON public.iscrizioni_eventi IS 'Lo stato	di un evento sportivo diventa CHIUSO quando si raggiunge un numero di giocatori	pari a quello previsto dalla categoria (la	presenza dell’arbitro è opzionale); in tal caso,	non	è più possibile effettuare iscrizioni all’evento.';
