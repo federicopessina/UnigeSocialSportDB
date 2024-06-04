1c1f86325b0657f935e29f4b03686bdc0e52e8b5
DarknessHunter <Anonghost.2@mail.ru>
2024-06-04 16:42:28 +0200

    Delete adds directory

--- adds/datacheck.sql
+++
@@ -1,2 +0,0 @@
-SELECT * FROM public.iscrizioni_eventi ie
-WHERE NOT EXISTS (SELECT 1 FROM public.eventi e WHERE e.id = ie."[eventi]_id");
--- adds/testing.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO public.utenti (email, username, password, nome, cognome, "[stati_utenti]_stato") VALUES ('test@example.com', 'testuser', 'password', 'Test', 'User', 'ATTIVO');
-SELECT * FROM public.utenti WHERE username = 'testuser';
