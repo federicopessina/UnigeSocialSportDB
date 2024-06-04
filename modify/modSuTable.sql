07d7394306d6f2d404c252a7b597c62557919a6c
DarknessHunter <Anonghost.2@mail.ru>
2024-06-04 16:42:40 +0200

    Delete modify directory

--- modify/modSuTable.sql
+++
@@ -1,7 +0,0 @@
---Per normalization 
-CREATE TABLE dettagli_eventi (
-    id_evento INT PRIMARY KEY,
-    programma TIMESTAMP,
-    luogo VARCHAR(100),
-    FOREIGN KEY (id_evento) REFERENCES eventi(id_evento)
-);
--- modify/tableLivelli.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Livelli (
-    livello_id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES Utenti(user_id),
-    sport_id INT REFERENCES Sport(sport_id),
-    livello INT CHECK (livello BETWEEN 0 AND 100) DEFAULT 60
-);
