2a047333c7dbe9ba0343d03448593faa8d229688
DarknessHunter <Anonghost.2@mail.ru>
2024-06-04 16:48:29 +0200

    Update and rename test_trigger.sql.txt to test_trigger.sql

--- tests/test_trigger.sql
+++ tests/test_trigger.sql
@@ -1,4 +1,2 @@
--- test_trigger.sql
--- Test per trigger_verifica_disponibilita_impianto
 INSERT INTO public.eventi (id, data_di_svolgimento, e_aperto, numero_massimo_iscritti, data_limite_disiscrizione, data_limite_iscrizione, "[sport]_categoria", "[utenti]_username", "[impianti]_nome")
 VALUES ('E1', '2024-06-15', true, 5, '2024-06-10', '2024-06-13', 'calcio', 'jdoe1', 'Stadio Luigi Ferraris');
